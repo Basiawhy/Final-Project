91e1d2e9857b722c0409163c44dba877cc8e2a49
Basiawhy <79459061+Basiawhy@users.noreply.github.com>
2021-03-05 20:56:52 +0100

    added forecast

--- javascript.js
+++ javascript.js
@@ -34,8 +34,55 @@ let formattedDate = `${currentDay}, ${currentMonth} ${currentDate}, ${currentYea
 let h2 = document.querySelector("h2");
 h2.innerHTML = formattedDate;
 
+function formatHours(timestamp) {
+    let date = new Date(timestamp);
+    let hours = date.getHours();
+    if (hours < 10) {
+        hours = `0${hours}`;
+    }
+    let minutes = date.getMinutes();
+    if (minutes < 10) {
+        minutes = `0${minutes}`;
+    }
+
+    return `${hours}:${minutes}`;
+}
+
+function displayForecast(response) {
+    console.log(response)
+    let forecastElement = document.querySelector("#forecast");
+    forecastElement.innerHTML = null;
+    let forecast = null;
+
+    for (let index = 0; index < 6; index++) {
+        forecast = response.data.list[index];
+        forecastElement.innerHTML += `
+          <div class="col-2">
+          <div class="card">
+           <div class="card-body">
+            <h5 class="card-title">
+              ${formatHours(forecast.dt * 1000)}
+            </h5>
+            <img
+              src="http://openweathermap.org/img/wn/${
+                forecast.weather[0].icon
+              }@2x.png"
+            />
+            <div class="weather-forecast-temperature">
+              <strong>
+                ${Math.round(forecast.main.temp_max)}°
+              </strong>
+               / ${Math.round(forecast.main.temp_min)}°
+            </div>
+            </div>
+            </div>
+          </div>
+        `;
+    }
+}
+
 function showTemperature(response) {
-    console.log(response.data.weather[0].icon)
+    console.log(response);
 
     celsjusTemperature = Math.round(response.data.main.temp);
     let feelsLikeTemperature = Math.round(response.data.main.feels_like);
@@ -76,6 +123,9 @@ function updateCity(event) {
 
     axios.get(`${url}&appid=${apiKey}`).then(showTemperature);
 
+    apiUrl = `https://api.openweathermap.org/data/2.5/forecast?q=${city}&appid=${apiKey}&units=metric`;
+    axios.get(apiUrl).then(displayForecast);
+
 }
 
 function search(city) {
@@ -83,13 +133,16 @@ function search(city) {
     let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
     axios.get(apiUrl).then(showTemperature)
 
-    //
+
     apiUrl = `https://api.openweathermap.org/data/2.5/forecast?q=${city}&appid=${apiKey}&units=metric`;
     axios.get(apiUrl).then(displayForecast);
 }
 
 function displayFahrenheit(event) {
     event.preventDefault;
+
+    celsjusLink.classList.remove("active");
+    fahrenheitLink.classList.add("active");
 
     let fahrenheitTemperature = (celsjusTemperature * 9) / 5 + 32;
     let temp = document.querySelector("#temp");
@@ -98,6 +151,9 @@ function displayFahrenheit(event) {
 
 function displayCelsjus(event) {
     event.preventDefault;
+
+    celsjusLink.classList.add("active");
+    fahrenheitLink.classList.remove("active");
 
     let temp = document.querySelector("#temp");
     temp.innerHTML = Math.round(celsjusTemperature);
